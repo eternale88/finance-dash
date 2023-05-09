b91c8aa61d85aadac4c5549c2b177a70060a945c
eternale88 <ethanrobbins@ymail.com>
2023-05-09 13:28:08 -0400

    backend routes and model for transactions

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
-// import transactionRoutes from "./routes/transaction.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
-// import Transaction from "./models/Transaction.js";
+import Transaction from "./models/Transaction.js";
 import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
@@ -27,6 +27,7 @@ app.use(cors());
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -42,6 +43,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     //Product.insertMany(products);
-    // Transaction.insertMany(transactions);
+    //Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
